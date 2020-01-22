617041f6ab48e5ebcefacb2cd475ab7b87900587
Sergio Salomao <salomao.leite@hotmail.com>
2020-01-21 22:05:51 -0300

    ajustes

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <q-toolbar-title>Quasar App</q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+        <q-item to="dashboard">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Dashboard</q-item-label>
+            <q-item-label caption></q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/clientes">
+          <q-item-section avatar>
+            <q-icon name="user" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Clientes</q-item-label>
+            <q-item-label caption>Informações dos clientes</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/atividades">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Atividades</q-item-label>
+            <q-item-label caption>Informações das atividades</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+export default {
+  name: 'MyLayout',
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  }
+};
+</script>
---
+++ src/pages/cliente/Cadastro.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page style="padding: 40px">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4">col</div>
+      <div class="col-xs-12 col-sm-6 col-md-4">col</div>
+      <div class="col-xs-12 col-sm-6 col-md-4">col</div>
+    </div>
+    <q-card>
+      <q-card-section>
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+          <div class="col-12 col-md-12">
+            <q-input outlined v-model="cliente.nome" label="Digite o nome" />
+          </div>
+          <q-input filled type="number" v-model="cliente.cpfcnpj" label="Your age *" />
+        </q-form>
+      </q-card-section>
+      <q-card-actions>
+        <div>
+          <q-btn label="Submit" @click.prevent="onSubmit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { QForm, QCard, QCardSection, QCardActions } from "quasar";
+import axios from "axios";
+export default {
+  name: "PageIndex",
+  components: {
+    QForm,
+    QCard,
+    QCardSection,
+    QCardActions
+  },
+  data() {
+    return {
+      cliente: {}
+    };
+  },
+  methods: {
+    onSubmit() {
+      const url = "http://192.168.1.224:8000/api/clientes";
+      axios.post(url, this.cliente).then(() => {});
+    },
+    onReset() {}
+  }
+};
+</script>
